c10670d0f13612be9934af4d35c78b8db31aafbb
PradnyaKuswara <pradnyakuswara24@gmail.com>
2024-05-16 11:38:01 +0700

    fixing ci

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -17,7 +17,7 @@
                         text of the printing and typesetting
                         industry. Lorem Ipsum
                         has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a
-                        galley</p>
+                        galle</p>
                 </div>
             </div>
         </div>

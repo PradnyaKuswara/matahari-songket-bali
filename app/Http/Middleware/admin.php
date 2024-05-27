c7b5636b4ef4c1ec7f046777c3c64f66b89cda7a
PradnyaKuswara <pradnyakuswara24@gmail.com>
2024-05-28 01:27:53 +0700

    fix cd

--- app/Http/Middleware/admin.php
+++ app/Http/Middleware/admin.php
@@ -17,7 +17,7 @@ class Admin
     public function handle(Request $request, Closure $next): Response
     {
         if (auth()->check()) {
-            if (! auth()->user()->isAdmin()) {
+            if (!auth()->user()->isAdmin()) {
                 Toaster::error('Not found');
 
                 return redirect()->back();

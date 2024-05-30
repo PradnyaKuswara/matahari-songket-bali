e99d1ca6ff3700a096968c54c1554200b5c46127
PradnyaKuswara <pradnyakuswara24@gmail.com>
2024-05-30 22:39:16 +0700

    fix rechaptcha

--- config/recaptcha.php
+++ config/recaptcha.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+    'google_recaptcha_url' => env('GOOGLE_RECAPTCHA_URL'),
     'google_recaptcha_key' => env('GOOGLE_RECAPTCHA_KEY'),
     'google_recaptcha_secret' => env('GOOGLE_RECAPTCHA_SECRET'),
 ];

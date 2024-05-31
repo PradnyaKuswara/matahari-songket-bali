86efb645c01991ff65ba9fec098c75523ec54462
PradnyaKuswara <pradnyakuswara24@gmail.com>
2024-05-31 18:42:42 +0700

    fix whatsaap service message

--- app/Services/WhatsappService.php
+++ app/Services/WhatsappService.php
@@ -12,7 +12,8 @@ class WhatsappService
         $twilioToken = config('twilio.twilio_auth_token');
         $twilioWhatsAppNumber = config('twilio.twilio_whatsapp_number');
         $recipientNumber = config('twilio.receiver_phone_number'); // Replace with the recipient's phone number in WhatsApp format (e.g., "whatsapp:+1234567890")
-        $message = 'Hi, this is a test message from Twilio WhatsApp API.';
+        //message to information shippinh to seller and direct link place order
+        $message = 'Hello, you have a new order with order ID: '.$order->generate_id.'. Please check your dashboard to process the order or click this link to view the order: '.route('seller.dashboard.shippings.index').'. Thank you!';
 
         $twilio = new Client($twilioSid, $twilioToken);
 

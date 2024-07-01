c0f9213056a295fb2b459337ff2a4ebae74878b0
PradnyaKuswara <pradnyakuswara24@gmail.com>
2024-07-01 17:12:38 +0700

    fix ui

--- resources/views/pages/admin-seller/orders/detail-order.blade.php
+++ resources/views/pages/admin-seller/orders/detail-order.blade.php
@@ -8,7 +8,7 @@
     <div>
         <ul class="flex space-x-2 rtl:space-x-reverse">
             <li>
-                <a href="javascript:;" class="text-primary hover:underline">Customer</a>
+                <a href="javascript:;" class="text-primary hover:underline">Dashboard</a>
             </li>
             <li class="before:content-['/'] ltr:before:mr-1 rtl:before:ml-1">
                 <span>Orders</span>
